5843ce593f1864a722fc5de3a3c3cf60cd27d754
Malte Swart <mswart@devtation.de>
2012-08-16 22:13:56 +0200

    wsgi: add cateens of hannover

--- wsgihandler.py
+++ wsgihandler.py
@@ -1,6 +1,7 @@
 #!python3
 
 from magdeburg import parse_url as magdeburg_parse
+from hannover import parse_url as hannover_parse
 
 providers = {
 	'magdeburg': { 
@@ -13,6 +14,24 @@ providers = {
 			'stendal': 'mensa-stendal/speiseplan/',
 			'halberstadt': 'mensa-halberstadt/speiseplan/',
 			'wernigerode': 'mensa-wernigerode/speiseplan/'
+		}
+	},
+	'hannover': {
+		'handler': hannover_parse,
+		'prefix': 'http://www.stwh-portal.de/mensa/index.php?format=txt&wo=',
+		'canteens': {
+			'hauptmensa': '2',
+			'hauptmensa-marktstand': '9',
+			'restaurant-ct': '10',
+			'contine': '3',
+			'pzh': '13',
+			'caballus': '1',
+			'tiho-tower': '0',
+			'hmtmh': '8',
+			'ricklinger-stadtweg': '6',
+			'kurt-schwitters-forum': '7',
+			'blumhardtstrasse': '14',
+			'herrenhausen': '12',
 		}
 	}
 }
