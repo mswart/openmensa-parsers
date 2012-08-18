c4f3e71d440ebac396264470ced524121c4e5523
Malte Swart <mswart@devtation.de>
2012-08-19 00:20:04 +0200

    hannover: Use helper and parse 'geschlossen' information

--- hannover.py
+++ hannover.py
@@ -3,6 +3,8 @@ from urllib.request import urlopen
 import re
 from xml.dom.minidom import Document
 
+from helper import OpenMensaCanteen
+
 day_regex = re.compile('(?P<date>\d{2}\.\d{2}\.\d{4})')
 price_regex = re.compile('(?P<price>\d+[,.]\d{2})€')
 note_regex = re.compile('\((?P<number>[a-z0-9]+?)\)')
@@ -15,63 +17,38 @@ def rolesGenerator():
 	while True:
 		yield 'other'
 
-def parse_week(url, canteen, output):
+def parse_week(url, canteen):
 	document = urlopen(url).read().decode('utf8').split('\n')
-	legends = { v.group('number'): v.group('value') for v in filter(lambda d: d, map(lambda v: legend_regex.match(v), document))}
+	legends = { v.group('number'): v.group('value') for v in map(lambda v: legend_regex.match(v), document) if v }
 	date = None
-	day = None
 	for line in document:
 		if not date:
 			test = day_regex.search(line)
-			if not test:
-				continue
-			else:
+			if test:
 				date = test.group('date')
-				day = output.createElement('day')
-				day.setAttribute('date', '{}-{}-{}'.format(date[6:10], date[3:5], date[0:2]))
-				continue
+			continue
+		if 'geschlossen' in line.lower():
+			canteen.setDayClosed(date)
 		if not line.startswith('>'):
-			if day and date is True:
-				canteen.appendChild(day)
 			date = None
 			continue
 		mealtest = meal_regex.search(line)
 		if not mealtest:
 			print('unable to parse category/meal: "{}"'.format(line))
 			continue
-		category = output.createElement('category')
-		category.setAttribute('name', mealtest.group('category').strip())
-		meal = output.createElement('meal')
-		name = output.createElement('name')
-		name.appendChild(output.createTextNode(mealtest.group('meal').strip()))
-		meal.appendChild(name)
+		category = mealtest.group('category').strip()
+		name = mealtest.group('meal').strip()
+		notes = []
 		for notematch in note_regex.findall(line):
-			if notematch not in legends: 
+			if notematch not in legends:
 				print('unknown legend: {}'.format(notematch))
 				continue
-			note = output.createElement('note')
-			note.appendChild(output.createTextNode(legends[notematch]))
-			meal.appendChild(note)
-		priceRoles = iter(rolesGenerator())
-		for pricematch in price_regex.findall(line):
-			price = output.createElement('price')
-			price.setAttribute('role', next(priceRoles))
-			price.appendChild(output.createTextNode(pricematch.strip().replace(',', '.')))
-			meal.appendChild(price)
-		category.appendChild(meal)
-		day.appendChild(category)
-		date = True
+			notes.append(legends[notematch])
+		canteen.addMeal(date, category, name, notes,
+				price_regex.findall(line), rolesGenerator)
 
 def parse_url(url):
-	output = Document()
-	openmensa = output.createElement('openmensa')
-	openmensa.setAttribute('version', '2.0')
-	openmensa.setAttribute('xmlns',"http://openmensa.org/open-mensa-v2")
-	openmensa.setAttribute('xmlns:xsi', "http://www.w3.org/2001/XMLSchema-instance")
-	openmensa.setAttribute('xsi:schemaLocation', "http://openmensa.org/open-mensa-v2 http://openmensa.org/open-mensa-v2.xsd")
-	output.appendChild(openmensa)
-	canteen = output.createElement('canteen')
-	openmensa.appendChild(canteen)
-	parse_week(url + '&wann=2', canteen, output)
-	parse_week(url + '&wann=3', canteen, output)
-	return '<?xml version="1.0" encoding="UTF-8"?>\n' + openmensa.toprettyxml(indent='  ')
+	canteen = OpenMensaCanteen()
+	parse_week(url + '&wann=2', canteen)
+	parse_week(url + '&wann=3', canteen)
+	return canteen.toXMLFeed()
