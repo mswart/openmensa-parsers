4144e50b3f614fe268c1601a6c9696bdc949c285
Malte Swart <mswart@devtation.de>
2013-05-09 13:50:46 +0200

    wuerzburg: handle no price information and other improvements
    * Format PEP8 compatible
    * fix exception on meals without price tag
    * Detect days as closed with 'geschlossen' in meal names (at the moment e.g.
      2013-05-17)
    * Uses LazyBuilder instead of OpenMensaCanteen (pyom)

--- wuerzburg.py
+++ wuerzburg.py
@@ -4,14 +4,15 @@ from bs4 import BeautifulSoup as parse
 import re
 import datetime
 
-from pyopenmensa.feed import OpenMensaCanteen
+from pyopenmensa.feed import LazyBuilder
 
 day_regex = re.compile('(?P<day>\d{2})\.(?P<month>\d{2})\.')
 price_regex = re.compile('(?P<price>\d+[,.]\d{2}) ?€')
 extra_regex = re.compile('[0-9,]+=(?P<note>\w+(\s|\w)*)')
 
+
 def parse_url(url):
-	canteen = OpenMensaCanteen()
+	canteen = LazyBuilder()
 	document = parse(urlopen(url).read())
 	for day_div in document.find_all('div', 'day') + document.find_all('article', attrs={'data-day': True}):
 		# parse date, warning: calculate year number needed
@@ -22,7 +23,7 @@ def parse_url(url):
 		else:
 			year = datetime.datetime.now().year
 			if datetime.datetime.now().month > int(date_test.group('month')):
-				year += 1 # date from next year
+				year += 1  # date from next year
 			date = "{}-{}-{}".format(year, date_test.group('month'), date_test.group('day'), )
 		if 'nodata' in day_div.attrs.get('class', []) or 'GESCHLOSSEN' in day_div.text:
 			canteen.setDayClosed(date)
@@ -31,13 +32,19 @@ def parse_url(url):
 			name = meal_article.find('div', 'title').text
 			if not name:
 				continue
+			if 'geschlossen' in name:
+				canteen.setDayClosed(date)
+				continue
 			category = meal_article.find('div', 'desc').text
-			notes = [ v['title'] for v in meal_article.find_all('div', 'theicon') if v['title'] ]
+			notes = [v['title'] for v in meal_article.find_all('div', 'theicon') if v['title']]
 			if meal_article.find('div', 'additive'):
-				notes += [ v[0] for v in extra_regex.findall(meal_article.find('div', 'additive').text) ]
+				notes += [v[0] for v in extra_regex.findall(meal_article.find('div', 'additive').text)]
 			price_div = meal_article.find('div', 'price')
+			if price_div is None:
+				canteen.addMeal(date, category, name, notes)
+				continue
 			prices = {}
-			for v, r in ( ('default', 'student'), ('bed', 'employee'), ('guest', 'other') ):
+			for v, r in (('default', 'student'), ('bed', 'employee'), ('guest', 'other')):
 				price = price_regex.search(price_div['data-' + v])
 				if price:
 					prices[r] = price.group('price')
@@ -46,6 +53,6 @@ def parse_url(url):
 					if canteen.hasMealsFor(date):
 						canteen.setDayClosed(date)
 					break
-			if prices and name:
+			if prices:
 				canteen.addMeal(date, category, name, notes, prices)
 	return canteen.toXMLFeed()
