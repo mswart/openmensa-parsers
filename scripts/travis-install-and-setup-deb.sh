5da6155400cbe4cf8afac99d1ea0aa9a9487c3ba
Malte Swart <mswart@devtation.de>
2013-10-11 01:45:33 +0200

    travis: try to build debian package 12

--- scripts/travis-install-and-setup-deb.sh
+++ scripts/travis-install-and-setup-deb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
-debi
-echo "  http: 127.0.0.1:9090" >> /etc/uwsgi/apps-enabled/openmensa-parsers.yml
+deb -i ../*.deb
+echo "    http: 127.0.0.1:9090" >> /etc/uwsgi/apps-enabled/openmensa-parsers.yml
 /etc/init.d/uwsgi restart openmensa-parsers
