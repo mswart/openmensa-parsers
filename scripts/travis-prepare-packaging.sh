4929c67aaf816399f6a3fbfb0d53a7b2e7ca2989
Malte Swart <mswart@devtation.de>
2013-10-11 02:52:57 +0200

    travis: try to build debian package 19

--- scripts/travis-prepare-packaging.sh
+++ scripts/travis-prepare-packaging.sh
@@ -2,6 +2,6 @@
 set -ex
 export DEBEMAIL=packages@devtation.de
 export DEBFULLNAME='Travis CI Build VM'
-dch --distribution precise --newversion=1.0+b$TRAVIS_BUILD_NUMBER "Package commit `git log -1 --oneline $TRAVIS_COMMIT`"
+dch --distribution precise --newversion=1.0+b$TRAVIS_BUILD_NUMBER-0 "Package commit `git log -1 --oneline $TRAVIS_COMMIT`"
 sed -i '/Maintainer/a \
 Uploaders: '"$DEBFULLNAME <$DEBEMAIL>" debian/control
