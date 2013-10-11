53aaff336592290e4e1d36cf0a014227b43a2149
Malte Swart <mswart@devtation.de>
2013-10-11 02:20:34 +0200

    travis: try to build debian package 14

---
+++ scripts/travis-prepare-packaging.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -ex
+export DEBEMAIL=packages@devtation.de
+export DEBFULLNAME='Travis CI Build VM'
+dch --distribution precise --newversion=1.0+b$TRAVIS_BUILD_NUMBER "Package commit `git log -1 --oneline $TRAVIS_COMMIT`"
+sed -i '/Maintainer/a \
+Changed-By: '"$DEBFULLNAME <$DEBEMAIL>" debian/control
