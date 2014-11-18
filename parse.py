20abf303e5283a6c787ed7ed28a4a3799c026df0
Malte Swart <mswart@devtation.de>
2014-11-18 17:23:52 +0100

    parse: usage and error handling improvements

--- parse.py
+++ parse.py
@@ -4,13 +4,16 @@ import sys
 
 if len(sys.argv) < 3:
     print('usage: {} <provider> <canteen>'.format(sys.argv[0]), file=sys.stderr)
+    sys.exit(1)
 provider = sys.argv[1]
 canteen = sys.argv[2]
 if provider not in providers:
-    print('unknown provider', file=sys.stderr)
-    print('registered provider:\n{}'.format('\n'.join(sorted(providers.keys()))), file=sys.stderr)
+    print('unknown provider "{}"'.format(provider), file=sys.stderr)
+    print('registered provider:\n  {}'.format('\n  '.join(sorted(providers.keys()))), file=sys.stderr)
+    sys.exit(2)
 if canteen not in providers[provider]['canteens']:
-    print('unknown canteen', file=sys.stderr)
-    print('registered canteen:\n{}'.format('\n'.join(sorted(providers[provider]['canteens'].keys()))), file=sys.stderr)
+    print('unknown canteen "{}"'.format(canteen), file=sys.stderr)
+    print('registered canteen:\n  {}'.format('\n  '.join(sorted(providers[provider]['canteens'].keys()))), file=sys.stderr)
+    sys.exit(3)
 
 print(parse(provider, canteen, len(sys.argv) > 3))
