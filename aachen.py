ccf3c74b572d88c2b438648b09194cb6449c5c77
Malte Swart <mswart@devtation.de>
2015-04-27 13:23:48 +0200

    aachen: port to bs4 4.0 only features, class_ was added in 4.1

--- aachen.py
+++ aachen.py
@@ -14,13 +14,13 @@ def parse_day(canteen, day, data):
     if note:
         canteen.setDayClosed(day)
         return
-    for menu in data.find(class_='menues').find_all('tr'):
+    for menu in data.find(attrs={'class':'menues'}).find_all('tr'):
         # category:
-        category = menu.find('td', class_='category').text.strip()
+        category = menu.find('td', attrs={'class':'category'}).text.strip()
         # split name and notes:
         name = ''
         notes = set()
-        for namePart in menu.find('td', class_='menue').children:
+        for namePart in menu.find('td', attrs={'class':'menue'}).children:
             if type(namePart) is NavigableString:
                 name += namePart.string
             elif type(namePart) is Tag:
@@ -28,7 +28,7 @@ def parse_day(canteen, day, data):
         name = name.strip()
         notes = [legend.get(n, n) for n in notes]
         # price:
-        price = menu.find('td', class_='price').text.strip()
+        price = menu.find('td', attrs={'class':'price'}).text.strip()
         # store data
         canteen.addMeal(day, category, name, notes, price)
 
