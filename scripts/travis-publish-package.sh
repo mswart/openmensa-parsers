37aede570625cc50a9c5de23c8f8e7364abd9ec8
Malte Swart <mswart@devtation.de>
2015-11-10 16:29:10 +0100

    travis: adjust to new upload url + print info about build deb

--- scripts/travis-publish-package.sh
+++ scripts/travis-publish-package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 tar --create ../openmensa-parsers_* | openssl enc -aes-256-cbc -e -pass env:TRANSFER_KEY > upload.bin
-wget --post-file upload.bin --output-document=- http://packages.devtation.de/travis-upload/openmensa-parsers
+wget --post-file upload.bin --output-document=- http://apt.dxtt.de/travis-upload/openmensa-parsers
