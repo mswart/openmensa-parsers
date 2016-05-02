4c092554fce8e744bcc6aea8bfc913a5c882e592
Christian Schabesberger <chris.schabesberger@mailbox.org>
2016-05-02 16:43:44 +0200

    fixed closed error in erlangen nuerenberg

--- erlangen_nuernberg.py
+++ erlangen_nuernberg.py
@@ -182,7 +182,11 @@ def parse_url(url, today=False):
 
     def get_pricing(tds, f, t):
         priceing = []
+        #sometimes we dont don't get 7 elements, than this might be a special day
+        if len(tds) < 7:
+            return None
         for i in range(f, t):
+            print(len(tds))
             raw_price = tds[i].string.strip()
             if raw_price == '':
                 return None
