9f8923ec968bfd363c04495a5891e53f7184d0be
Klemens Schölhorn <klemens@schoelhorn.eu>
2016-06-28 00:59:42 +0200

    Add python3-lxml to the travis dependencies
    This package is already a requirement for the debian package and useful
    for parsing xml with beautifulsoup.

--- scripts/travis-install-deps.sh
+++ scripts/travis-install-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 apt-get update -qq
-apt-get install -qq python3 python3-bs4 uwsgi uwsgi-plugin-python3 devscripts debhelper
+apt-get install -qq python3 python3-bs4 python3-lxml uwsgi uwsgi-plugin-python3 devscripts debhelper
