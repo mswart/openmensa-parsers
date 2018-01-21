39ef9c7a397bcf7d9631b5d1c6b50f9e5c852f5f
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-01-21 16:03:03 +0100

    Add copyable output on failed regression test

--- parsers/tests/regression_test.py
+++ parsers/tests/regression_test.py
@@ -17,7 +17,9 @@ def test_parse_url(parser, canteen):
     result = parse_mocked(parser, canteen)
     with open(get_snapshot_result_path(parser, canteen), encoding='utf-8') as result_file:
         expected_result = result_file.read()
-        assert result == expected_result
+        assert result == expected_result, ("Actual result:\n"
+                                           + result
+                                           + "\n------ Diff: ------\n")
 
 
 def parse_mocked(parser, canteen):
