626ce6bf1b435b857364803ff2a74bcdddb79881
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-01-21 16:10:22 +0100

    Respect encoding during snapshot upgrade

--- parsers/tests/update_snapshots.py
+++ parsers/tests/update_snapshots.py
@@ -1,8 +1,11 @@
 import os
 from urllib.request import urlretrieve
 
-from parsers.tests.regression_test import parse_mocked, parsers_to_test, get_snapshot_website_path, \
-    get_snapshot_result_path, get_canteen_url
+from parsers.tests.regression_test import get_canteen_url, \
+    get_snapshot_result_path, \
+    get_snapshot_website_path, \
+    parse_mocked, \
+    parsers_to_test
 
 base_directory = os.path.dirname(os.path.realpath(__file__))
 
@@ -17,7 +20,7 @@ def generate_all_snapshots(canteens):
         urlretrieve(url, snapshot_website_path)
 
         snapshot_result_path = get_snapshot_result_path(parser, canteen)
-        with open(snapshot_result_path, 'w') as result_file:
+        with open(snapshot_result_path, 'w', encoding='utf-8') as result_file:
             result = parse_mocked(parser, canteen)
             result_file.write(result)
 
