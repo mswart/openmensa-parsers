0abb1f1701d32675885a1039c19ef756f47c7fd6
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-03-03 14:56:18 +0100

    Implement NotesBuilder

--- model/model_helpers.py
+++ model/model_helpers.py
@@ -1,4 +1,4 @@
-from model.openmensa_model import Prices
+from model.openmensa_model import Notes, Prices
 
 
 class PricesBuilder:
@@ -31,3 +31,25 @@ class PricesBuilder:
             if supplement is not None
         }
         return Prices(**actual_price_kwargs)
+
+
+class NotesBuilder:
+    def __init__(self, legend, ignore_not_found=False):
+        """
+
+        :param dict[string, string] legend:
+        """
+        self.legend = legend
+        self.ignore_not_found = ignore_not_found
+
+    def build_notes(self, note_keys):
+        """
+
+        :param list[string] note_keys:
+        :rtype: Notes
+        """
+        if self.ignore_not_found:
+            note_list = [self.legend[key] for key in note_keys if key in self.legend]
+        else:
+            note_list = [self.legend[key] if key in self.legend else key for key in note_keys]
+        return Notes(note_list)
--- model/model_helpers_test.py
+++ model/model_helpers_test.py
@@ -1,9 +1,10 @@
 import pytest
 
-from model.model_helpers import PricesBuilder
-from model.openmensa_model import Prices
+from model.model_helpers import NotesBuilder, PricesBuilder
+from model.openmensa_model import Notes, Prices
 
 
+# PricesBuilder
 def test_given_all_roles_then_builds_prices():
     builder = PricesBuilder(other=0, pupils=240, students=120, employees=1)
     prices = builder.build_prices(100)
@@ -21,3 +22,35 @@ def test_given_some_roles_then_builds_prices_with_those_roles_only():
     prices = builder.build_prices(100)
 
     assert prices == Prices(pupils=223, employees=421)
+
+
+# NotesBuilder
+def test_given_legend_and_keys_then_builds_notes():
+    legend = {
+        '1': 'First',
+        '2': 'Second',
+        '3': 'Third',
+    }
+    builder = NotesBuilder(legend)
+    note_keys = ['1', '3']
+    notes = builder.build_notes(note_keys)
+
+    assert notes == Notes(['First', 'Third'])
+
+
+def test_given_keys_not_in_legend_then_keeps_them_by_default():
+    legend = {'A': 'map me'}
+    builder = NotesBuilder(legend)
+    note_keys = ['keep me', 'A']
+    notes = builder.build_notes(note_keys)
+
+    assert notes == Notes(['keep me', 'map me'])
+
+
+def test_given_flag_to_ignore_keys_not_found_then_ignores_them():
+    legend = {'1': 'map me'}
+    builder = NotesBuilder(legend, ignore_not_found=True)
+    note_keys = ['ignore me', '1']
+    notes = builder.build_notes(note_keys)
+
+    assert notes == Notes(['map me'])
