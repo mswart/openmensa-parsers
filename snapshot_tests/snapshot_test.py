c9b7fe24501a009e42d75475492220baea4835b7
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-04-06 17:22:18 +0200

    Run tests on all existing snapshots

--- snapshot_tests/snapshot_test.py
+++ snapshot_tests/snapshot_test.py
@@ -5,9 +5,13 @@ import pytest
 from config import parsers
 
 base_directory = os.path.dirname(os.path.realpath(__file__))
+snapshots_directory = os.path.join(base_directory, 'snapshots')
 
+# List all snapshots subdirectories, e. g. `aachen/academica` becomes ("aachen", "academica").
 parsers_to_test = [
-    ('aachen', 'academica'),
+    (parser, canteen)
+    for parser in os.listdir(os.path.join(snapshots_directory))
+    for canteen in os.listdir(os.path.join(snapshots_directory, parser))
 ]
 
 
@@ -20,4 +24,4 @@ def test_parse_url(parser, canteen):
 
 
 def get_snapshot_result_path(parser, canteen):
-    return os.path.join(base_directory, 'snapshots', parser, canteen, 'snapshot-result.xml')
+    return os.path.join(snapshots_directory, parser, canteen, 'snapshot-result.xml')
