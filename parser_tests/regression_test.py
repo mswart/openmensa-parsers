37c16139a0200a871438807e6baf7393f4648c20
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-04-06 16:57:26 +0200

    Simplify regression tests and snapshots

--- parser_tests/regression_test.py
+++ parser_tests/regression_test.py
@@ -1,6 +1,4 @@
-import json
 import os
-from unittest import mock
 
 import pytest
 
@@ -15,31 +13,11 @@ parsers_to_test = [
 
 @pytest.mark.parametrize("parser,canteen", parsers_to_test)
 def test_parse_url(parser, canteen):
-    result = parse_mocked(parser, canteen)
+    result = parsers[parser].parse('', canteen, 'full.xml')
     with open(get_snapshot_result_path(parser, canteen), encoding='utf-8') as result_file:
         expected_result = result_file.read()
         assert result == expected_result
 
 
-def parse_mocked(parser, canteen):
-    snapshot_website_path = get_snapshot_website_path(parser, canteen)
-    with open(snapshot_website_path) as snapshot_file:
-        website_snapshots = json.load(snapshot_file)
-
-        def mock_response(actual_url):
-            class MockResponse:
-                def read(self):
-                    return website_snapshots[actual_url]
-
-            return MockResponse()
-
-    with mock.patch('urllib.request.urlopen', mock_response):
-        return parsers[parser].parse('', canteen, 'full.xml')
-
-
-def get_snapshot_website_path(parser, canteen):
-    return os.path.join(base_directory, parser, canteen, 'snapshot-website.json')
-
-
 def get_snapshot_result_path(parser, canteen):
-    return os.path.join(base_directory, parser, canteen, 'snapshot-result.xml')
+    return os.path.join(base_directory, 'snapshots', parser, canteen, 'snapshot-result.xml')
