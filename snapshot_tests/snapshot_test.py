7db4386dcda5758ac10fc36b586d6611e27559c3
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-04-14 22:29:03 +0200

    Input url into parser when testing

--- snapshot_tests/snapshot_test.py
+++ snapshot_tests/snapshot_test.py
@@ -3,7 +3,7 @@ import os
 import pytest
 
 from config import parsers
-from .utils import get_snapshot_result_path, snapshots_directory
+from .utils import get_snapshot_result_path, snapshots_directory, get_canteen_url
 
 # List all snapshots subdirectories, e. g. `aachen/academica` becomes ("aachen", "academica").
 cities = os.listdir(snapshots_directory) if os.path.exists(snapshots_directory) else []
@@ -16,7 +16,8 @@ parsers_to_test = [
 
 @pytest.mark.parametrize("parser,canteen", parsers_to_test)
 def test_parse_url(parser, canteen):
-    result = parsers[parser].parse('', canteen, 'full.xml')
+    url = get_canteen_url(parser, canteen)
+    result = parsers[parser].parse(url, canteen, 'full.xml')
     with open(get_snapshot_result_path(parser, canteen), encoding='utf-8') as result_file:
         expected_result = result_file.read()
         assert result == expected_result
